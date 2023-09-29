69b614c43ae6f2a00f3b133ce7726f8918d9aca1
Mark <hansen-mark@hotmail.com>
2023-09-29 19:43:23 -0400

    Set up Tag Modal with Svelte.

---
+++ src/TagForm.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let value: string;
+	export let option: string;
+	export let closeModal: () => void;
+	export let submission: (value: string, option: string) => void;
+
+	function onSubmit(e: Event) {
+		e.preventDefault();
+		submission(value, option);
+	}
+</script>
+
+<span>
+	If you add multiple tags, separate them with commas. Do not add '#'.
+</span>
+<form on:submit={onSubmit} class="modal-form">
+	<select>
+		<option selected={option === "inline"}>Inline</option>
+		<option selected={option === "yaml"}>YAML</option>
+	</select>
+	<input id="tagInput" type="text" bind:value />
+	<div class="modal-button-container">
+		<button type="submit" class="mod-cta">Submit</button>
+		<button on:click={closeModal}>Cancel</button>
+	</div>
+</form>
--- src/TagModal.ts
+++ src/TagModal.ts
@@ -1,10 +1,102 @@
+// import { App, Modal, TAbstractFile, TFolder } from "obsidian";
+
+// export class TagModal extends Modal {
+// 	default: string = "";
+// 	base: TFolder | TAbstractFile[];
+// 	option: string = "inline";
+// 	submission: (obj: any, string: string, setting: string) => void;
+
+// 	constructor(
+// 		app: App,
+// 		base: TFolder | TAbstractFile[],
+// 		option: string = "inline",
+// 		submission: (obj: any, string: string, setting: string) => void
+// 	) {
+// 		super(app);
+
+// 		//Removes potential spaces in file names.
+// 		if (base instanceof TFolder) {
+// 			this.default = `${base.name.replace(" ", "-")}`;
+// 		}
+
+// 		this.base = base;
+// 		this.submission = submission;
+// 		this.option = option;
+// 	}
+
+// 	onSubmit(e: Event, input: string, option: string): void {
+// 		e.preventDefault();
+
+// 		//Trim any spaces to prevent splits in tags.
+// 		const trimmed = input.replace(/ /g, "");
+
+// 		this.submission(this.base, trimmed, option);
+// 		this.close();
+// 	}
+
+// 	onOpen(): void {
+// 		this.modalEl.addClass("modal");
+
+// 		const { contentEl, titleEl } = this;
+
+// 		//Create text.
+// 		titleEl.createEl("h2", { text: "Please type in a tag." });
+// 		contentEl.createEl("span", {
+// 			text: "If you add multiple tags, separate them with commas. Do not add '#'.",
+// 		});
+
+// 		//Create form object.
+// 		contentEl.createEl("form", { cls: "modal-form" }, (formEl) => {
+// 			//Select object to let user toggle between inline and YAML.
+// 			let selectEl = formEl.createEl("select", { cls: "select" });
+// 			selectEl.createEl("option", {
+// 				value: "inline",
+// 				text: "Inline",
+// 				attr: { selected: this.option === "inline" ? "selected" : null },
+// 			});
+// 			selectEl.createEl("option", {
+// 				value: "yaml",
+// 				text: "YAML",
+// 				attr: { selected: this.option === "yaml" ? "selected" : null },
+// 			});
+
+// 			let inputEl = formEl.createEl("input", {
+// 				value: this.default,
+// 				attr: { id: "tagInput" },
+// 			});
+// 			formEl.createDiv("modal-button-container", (buttonEl) => {
+// 				let btnSubmit = buttonEl.createEl("button", {
+// 					text: "Submit",
+// 					type: "submit",
+// 					cls: "mod-cta",
+// 				});
+
+// 				let btnCancel = buttonEl.createEl("button", {
+// 					text: "Cancel",
+// 					type: "cancel",
+// 				});
+// 				btnCancel.addEventListener("click", () => this.close());
+// 			});
+
+// 			formEl.addEventListener("submit", (e) =>
+// 				this.onSubmit(e, inputEl.value, selectEl.value)
+// 			);
+
+// 			//Set focus on the input after the DOM has been updated.
+// 			setTimeout(() => inputEl.focus(), 0);
+// 		});
+// 	}
+// }
+
 import { App, Modal, TAbstractFile, TFolder } from "obsidian";
+import TagForm from "./TagForm.svelte";
 
 export class TagModal extends Modal {
 	default: string = "";
 	base: TFolder | TAbstractFile[];
 	option: string = "inline";
 	submission: (obj: any, string: string, setting: string) => void;
+	component: TagForm;
 
 	constructor(
 		app: App,
@@ -24,66 +116,25 @@ export class TagModal extends Modal {
 		this.option = option;
 	}
 
-	onSubmit(e: Event, input: string, option: string): void {
-		e.preventDefault();
+	async onOpen() {
+		this.titleEl.createEl("h2", { text: "Please type in a tag." });
 
+		this.component = new TagForm({
+			target: this.contentEl,
+			props: {
+				value: this.default,
+				option: this.option,
+				closeModal: () => this.close(),
+				submission: this.onSubmit,
+			},
+		});
+	}
+
+	onSubmit(input: string, option: string): void {
 		//Trim any spaces to prevent splits in tags.
 		const trimmed = input.replace(/ /g, "");
 
 		this.submission(this.base, trimmed, option);
 		this.close();
-	}
-
-	onOpen(): void {
-		this.modalEl.addClass("modal");
-
-		const { contentEl, titleEl } = this;
-
-		//Create text.
-		titleEl.createEl("h2", { text: "Please type in a tag." });
-		contentEl.createEl("span", {
-			text: "If you add multiple tags, separate them with commas. Do not add '#'.",
-		});
-
-		//Create form object.
-		contentEl.createEl("form", { cls: "modal-form" }, (formEl) => {
-			//Select object to let user toggle between inline and YAML.
-			let selectEl = formEl.createEl("select", { cls: "select" });
-			selectEl.createEl("option", {
-				value: "inline",
-				text: "Inline",
-				attr: { selected: this.option === "inline" ? "selected" : null },
-			});
-			selectEl.createEl("option", {
-				value: "yaml",
-				text: "YAML",
-				attr: { selected: this.option === "yaml" ? "selected" : null },
-			});
-
-			let inputEl = formEl.createEl("input", {
-				value: this.default,
-				attr: { id: "tagInput" },
-			});
-			formEl.createDiv("modal-button-container", (buttonEl) => {
-				let btnSubmit = buttonEl.createEl("button", {
-					text: "Submit",
-					type: "submit",
-					cls: "mod-cta",
-				});
-
-				let btnCancel = buttonEl.createEl("button", {
-					text: "Cancel",
-					type: "cancel",
-				});
-				btnCancel.addEventListener("click", () => this.close());
-			});
-
-			formEl.addEventListener("submit", (e) =>
-				this.onSubmit(e, inputEl.value, selectEl.value)
-			);
-
-			//Set focus on the input after the DOM has been updated.
-			setTimeout(() => inputEl.focus(), 0);
-		});
 	}
 }
