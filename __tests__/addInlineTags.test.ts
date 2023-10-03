c6c80708b85b976269c78405bcb299466a3b3eca
Mark <hansen-mark@hotmail.com>
2023-10-02 21:16:41 -0400

    Updated Jest config.

--- __tests__/addInlineTags.test.ts
+++ __tests__/addInlineTags.test.ts
@@ -1,5 +1,20 @@
-it('Adds tags to all files in a folder', async () => {
-    
-})
+import MultiTagPlugin from "src/main";
+import { App, Plugin, getAllTags } from "obsidian";
 
-export {}
+// it("Adds tags to all files in a folder", async () => {});
+
+// it("Adds tags to all selected files", async () => {
+// 	//Check that 3rd file was not tagged.
+// });
+
+// it("Adds tags to the bottom of a note", async () => {});
+
+// it("Adds tags to the top of a note", async () => {});
+
+// it("Trims out spaces from tags", async () => {});
+
+it("Can run something from Obsidian.", async () => {
+	// console.log(getAllTags())
+});
+
+export {};
--- __tests__/setupTests.ts
+++ __tests__/setupTests.ts
@@ -1,12 +1,14 @@
-import { App } from "obsidian";
-//Before test starts, create a new folder and 3 blank files to test on.
-beforeAll(() => {
-    //Search for file-creating functions in Obsidian.
-    
-});
-//After each test, clear the files of all data.
-afterEach(() => {});
-//After all tests, delete the folder and files.
-afterAll(() => {});
+// import { App, Plugin } from "obsidian";
+// //Before test starts, create a new folder and 3 blank files to test on.
+
+// const app = new App();
+// beforeAll(() => {
+// 	//Search for file-creating functions in Obsidian.
+// 	console.log(app.vault.adapter.getName());
+// });
+// //After each test, clear the files of all data.
+// afterEach(() => {});
+// //After all tests, delete the folder and files.
+// afterAll(() => {});
 
 export {};
